99d0480278c4bf2ad3f9e2a6be784a8a5d333022
hpejak <0PAnT4oMiM@@>
2020-09-14 22:58:06 +0200

    cmp-databinding-start  View Encapsulation

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'testServer', name: 'Server_1', content: 'Test stuff'}];
+  serverElements = [{type: 'server', name: 'Server_1', content: 'Test stuff'}];
 
   onServerAdded(serverData: {name: string, content: string}) {
     this.serverElements.push({
