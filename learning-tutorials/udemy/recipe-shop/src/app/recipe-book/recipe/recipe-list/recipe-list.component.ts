be25821f65ff2883884546d4389ab312bcc10215
Pejak <pejak.hrvoje@gmail.com>
2021-11-27 11:24:02 +0100

    recipe-list added

--- learning-tutorials/udemy/recipe-shop/src/app/recipe-book/recipe/recipe-list/recipe-list.component.ts
+++ learning-tutorials/udemy/recipe-shop/src/app/recipe-book/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Demo Recipe',
+      'Just a test',
+      'https://yt3.ggpht.com/ytc/AKedOLSQYSjN--Nhic1c9BD1elMd0EUdUGeWkn4FfbfkOQ=s900-c-k-c0x00ffffff-no-rj'
+    )];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
