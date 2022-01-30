4bcc5e65a4f70d9886f8869b15a291baf3fe5a8b
Pejak <pejak.hrvoje@gmail.com>
2022-01-30 23:11:02 +0100

    Assignment Databinding finished

--- learning-tutorials/udemy/cmp-databinding-assignment-start/src/app/app.component.ts
+++ learning-tutorials/udemy/cmp-databinding-assignment-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(event: number) {
+    if (event % 2 === 0) {
+      this.evenNumbers.push(event);
+    } else {
+      this.oddNumbers.push(event);
+    }
+  }
 }
