ec3a51bd02bda0c01df2350bd98486d5e4eeff5c
Pejak <pejak.hrvoje@gmail.com>
2022-02-19 19:42:00 +0100

    services finished

--- learning-tutorials/udemy/services-start/src/app/accounts.service.ts
+++ learning-tutorials/udemy/services-start/src/app/accounts.service.ts
@@ -1,3 +1,7 @@
+import {LoggingService} from './logging.service';
+import {EventEmitter, Injectable} from '@angular/core';
+
+@Injectable()
 export class AccountsService {
   accounts = [
     {
@@ -5,7 +9,7 @@ export class AccountsService {
       status: 'active'
     },
     {
-      name: 'Testaccount',
+      name: 'Test account',
       status: 'inactive'
     },
     {
@@ -14,11 +18,19 @@ export class AccountsService {
     }
   ];
 
+
+  constructor(private loggingService: LoggingService) {
+  }
+
+  statusUpdate = new EventEmitter<string>();
+
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
--- learning-tutorials/udemy/services-start/src/app/new-account/new-account.component.ts
+++ learning-tutorials/udemy/services-start/src/app/new-account/new-account.component.ts
@@ -1,22 +1,22 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import {LoggingService} from '../logging.service';
 import {AccountsService} from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
 
-  constructor(private loggingService: LoggingService, private accountService: AccountsService) {
+  constructor(private accountService: AccountsService) {
+    this.accountService.statusUpdate.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
