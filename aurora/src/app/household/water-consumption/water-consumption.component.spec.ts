ebdd51496ef17a048df931374cbce684b66e4a36
Pejak <pejak.hrvoje@gmail.com>
2022-10-30 21:07:59 +0100

    household anda water-consumption added navigation hover

---
+++ aurora/src/app/household/water-consumption/water-consumption.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WaterConsumptionComponent } from './water-consumption.component';
+
+describe('WaterConsumptionComponent', () => {
+  let component: WaterConsumptionComponent;
+  let fixture: ComponentFixture<WaterConsumptionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WaterConsumptionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WaterConsumptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ aurora/src/app/household/water-consumption/water-consumption.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-water-consumption',
+  templateUrl: './water-consumption.component.html',
+  styleUrls: ['./water-consumption.component.css']
+})
+export class WaterConsumptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
