370b707e85ee3cd5bb6b67ae3b69d841216ddc66
Pejak <pejak.hrvoje@gmail.com>
2022-10-30 12:57:22 +0100

    router.navigate added

--- aurora/src/app/benjamin/benjamin.component.ts
+++ aurora/src/app/benjamin/benjamin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-benjamin',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BenjaminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onBenjaminWeightClick() {
+    this.router.navigate(["/benjamin-weight"])
+  }
 }
