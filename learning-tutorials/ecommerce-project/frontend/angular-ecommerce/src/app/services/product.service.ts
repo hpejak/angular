b667148f4c6b9a93366718b807b733876d2eb7a9
Pejak <pejak.hrvoje@gmail.com>
2022-11-01 12:12:38 +0100

    product search finished

--- learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/services/product.service.ts
+++ learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -18,18 +18,24 @@ export class ProductService {
   }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
-
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map((response: GetResponseProducts) => response._embedded.products)
-    );
-
+    return this.getProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map((response: GetResponseProductCategory) => response._embedded.productCategory)
+    );
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string) {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map((response: GetResponseProducts) => response._embedded.products)
     );
   }
 }
