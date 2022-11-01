cb059d6f68590656baa1cdcf23c5e5f24b52dc6c
Pejak <pejak.hrvoje@gmail.com>
2022-11-01 22:59:10 +0100

    ecommerce @ts-ignore added

--- learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/services/product.service.ts
+++ learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Product} from "../common/product";
+// @ts-ignore
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {ProductCategory} from "../common/product-category";
