e3b8f63809397880692271fb7430ddaa6c093501
Hrvoje <hrvoje.pejak@bulbtech.com>
2022-11-09 15:15:26 +0100

    products update

--- learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/common/product.ts
+++ learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,12 +1,12 @@
 export class Product {
-  id: number| undefined;
-  sku: string | undefined;
-  name: string | undefined;
-  description: string | undefined;
-  unitPrice: number | undefined;
-  imageUrl: string | undefined;
-  active: boolean | undefined;
-  unitsInStock: number | undefined;
-  dateCreated: string | undefined;
-  lastUpdate: string | undefined;
+  id!: number;
+  sku!: string;
+  name!: string;
+  description!: string;
+  unitPrice!: number;
+  imageUrl!: string;
+  active!: boolean;
+  unitsInStock!: number;
+  dateCreated!: string;
+  lastUpdate!: string;
 }
