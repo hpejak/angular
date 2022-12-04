ce093e5b5fcd16b41b4bdcdc27301ed3507e3da8
Pejak <pejak.hrvoje@gmail.com>
2022-12-04 17:43:10 +0100

    ecommerce pagination for list components finished

--- learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/compomemts/product-list/product-list.component.ts
+++ learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/compomemts/product-list/product-list.component.ts
@@ -15,7 +15,7 @@ export class ProductListComponent implements OnInit {
   searchMode: boolean = false;
 
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
 
@@ -76,5 +76,11 @@ export class ProductListComponent implements OnInit {
         this.products = data;
       }
     );
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
