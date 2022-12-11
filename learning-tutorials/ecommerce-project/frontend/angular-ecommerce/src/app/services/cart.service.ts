60fa0133bb75d9c98b422c39350f5615cc64e410
Pejak <pejak.hrvoje@gmail.com>
2022-12-11 22:26:32 +0100

    cart service added

---
+++ learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,67 @@
+import {Injectable} from '@angular/core';
+import {CartItem} from "../common/cart-item";
+import {Subject} from "rxjs";
+import {Product} from "../common/product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() {
+  }
+
+  addToCart(theCartItem: CartItem) {
+
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = new CartItem(new Product());
+
+    if (this.cartItems.length > 0) {
+
+      for (let tempCartItem of this.cartItems) {
+        if (tempCartItem.id === theCartItem.id) {
+          existingCartItem = tempCartItem;
+          break;
+        }
+      }
+      alreadyExistsInCart = (existingCartItem != undefined);
+    }
+
+    if (alreadyExistsInCart) {
+      existingCartItem.quantity++;
+    } else {
+      this.cartItems.push(theCartItem);
+    }
+    this.computeCartTotals();
+  }
+
+  private computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    this.cartItems.forEach(currentCartItem => {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    })
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    this.logCardData(totalPriceValue, totalQuantityValue);
+  }
+
+  private logCardData(totalPriceValue: number, totalQuantityValue: number) {
+    console.debug('Contents of the cart')
+    this.cartItems.forEach(cartItem => {
+      const subTotalPrice = cartItem.quantity * cartItem.unitPrice;
+      console.debug(`name: ${cartItem.name}, quantity: ${cartItem.quantity}, unitPrice: ${cartItem.unitPrice},
+      subtotalPrice: ${subTotalPrice}`);
+      console.debug(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+      console.debug('-- ----')
+    })
+  }
+}
