cb1f9bb7d28f22e1fd44516c9b2e2ee4f8fde0ab
Pejak <pejak.hrvoje@gmail.com>
2023-03-26 19:42:37 +0200

    checkout subscription BehaviorSubject

--- learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ learning-tutorials/ecommerce-project/frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import {Luv2ShopFormService} from "../../services/luv2-shop-form.service";
 import {Country} from "../../common/country";
 import {State} from "../../common/state";
 import {Luv2ShopValidators} from "../../validators/luv2-shop-validators";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-checkout',
@@ -24,10 +25,16 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
-  constructor(private formBuilder: FormBuilder, private luv2ShopFormService: Luv2ShopFormService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private luv2ShopFormService: Luv2ShopFormService,
+    private cartService: CartService) {
   }
 
   ngOnInit(): void {
+
+    this.reviewCardDetails();
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('',
@@ -250,5 +257,17 @@ export class CheckoutComponent implements OnInit {
         formGroup.get('state').setValue(data[0]);
       }
     );
+  }
+
+  reviewCardDetails() {
+
+    this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    );
+
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    );
+
   }
 }
